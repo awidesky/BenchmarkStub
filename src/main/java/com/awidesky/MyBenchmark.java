341d8a07fb14b290c813548886068158b4dfeab0
Eugene Hong <awidesky@naver.com>
2023-02-02 21:50:39 +0900

    add test

--- src/main/java/com/awidesky/MyBenchmark.java
+++ src/main/java/com/awidesky/MyBenchmark.java
@@ -31,21 +31,25 @@
 
 package com.awidesky;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
 import java.util.concurrent.TimeUnit;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
-import org.openjdk.jmh.infra.Blackhole;
 
 /*
  * mvn archetype:generate -DinteractiveMode=false -DarchetypeGroupId=org.openjdk.jmh -DarchetypeArtifactId=jmh-java-benchmark-archetype -DgroupId=com.awidesky -DartifactId=BenchmarkStub -Dversion=1.0
@@ -63,8 +67,8 @@ import org.openjdk.jmh.infra.Blackhole;
  * 
  * */
 
-@Warmup(iterations = 3) 		// Warmup Iteration = 3
-@Measurement(iterations = 5)
+@Warmup(iterations = 1) 		// Warmup Iteration = 3
+@Measurement(iterations = 3)
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
@@ -72,18 +76,60 @@ import org.openjdk.jmh.infra.Blackhole;
 @Fork(value = 2)
 public class MyBenchmark {
 
-    @Param({ "10000000" })
-    private int N;
-
-    @Setup(Level.Trial)
-    public void setup() {
-        //setup
-    }
-	 
-    @Benchmark
-    public void test(Blackhole bh) {
-    	//benchmark
-    }
+	@Benchmark
+	public static void KB516() {
+		downloadCompact(516 * 1024);
+	}
+	@Benchmark
+	public static void M1() {
+		downloadCompact(1024 * 1024);
+	}
+	@Benchmark
+	public static void MB2() {
+		downloadCompact(2 * 1024 * 1024);
+	}
     
+	public static void download(int bufsize) {
+		if(new File(".\\ffmpeg.zip").exists()) new File(".\\ffmpeg.zip").delete();
+		
+		try (ReadableByteChannel in = Channels.newChannel(new URL("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip").openStream());
+				FileChannel out = new FileOutputStream(new File(".\\ffmpeg.zip")).getChannel()) {
+			ByteBuffer bytebuf = ByteBuffer.allocateDirect(bufsize);
+			while (in.read(bytebuf) > 0 || bytebuf.position() > 0) {
+				// flip the buffer which set the limit to current position, and position to 0.
+				bytebuf.flip();
+				out.write(bytebuf); // Write data from ByteBuffer to file
+				bytebuf.compact(); // For the next read
+			}
+		} catch (IOException e) {
+			// TODO
+			e.printStackTrace();
+		}
+	}
+    
+  	public static void downloadCompact(int bufsize) {
+  		if(new File(".\\ffmpeg.zip").exists()) new File(".\\ffmpeg.zip").delete();
+  		
+  		try (ReadableByteChannel in = Channels.newChannel(new URL("https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-essentials.zip").openStream());
+  				FileChannel out = new FileOutputStream(new File(".\\ffmpeg.zip")).getChannel()) {
+  			ByteBuffer bytebuf = ByteBuffer.allocateDirect(bufsize);
+  			
+			boolean eof = false;
+  			while (true) {
+  				while(bytebuf.hasRemaining() && !eof) {
+  					eof = in.read(bytebuf) == -1;
+  				}
+				// flip the buffer which set the limit to current position, and position to 0.
+				bytebuf.flip();
+				while(bytebuf.hasRemaining()) out.write(bytebuf); // Write data from ByteBuffer to file
+				bytebuf.clear();
+				if(eof) break;
+			}
+  		} catch (IOException e) {
+  			// TODO
+  			e.printStackTrace();
+  		}
+  	}
 
+	
 }
